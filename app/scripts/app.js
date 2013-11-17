4cee19a8e7f83bfba21068e66e6cf9ac8f8ff7bb
owen chen <owen0215@gmail.com>
2013-11-17 20:52:04 +0800

    update angular to 1.2.1 and upgrade other components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,15 +4,20 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('testYeoApp', [
-  'ngRoute',
+  'ui.router',
   'testYeoApp.filters',
   'testYeoApp.services',
   'testYeoApp.directives',
   'testYeoApp.controllers',
   'ui.bootstrap',
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'views/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'views/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('view1', {url:'/view1', templateUrl: 'views/partial1.html', controller: 'MyCtrl1'});
+  $stateProvider.state('view2', {url:'/view2', templateUrl: 'views/partial2.html', controller: 'MyCtrl2'});
+  $urlRouterProvider.otherwise('/view1');
+}])
+.run(['$rootScope', '$state', '$stateParams',
+      function ($rootScope,   $state,   $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+      }]);
